06222d10f06bb5ec6c68af7a6df2a55b7b3be64f
constdreamcoder <scdreamricher@gmail.com>
2025-01-07 12:40:12 +0900

    [Feat] 도시 Repository 구현체 구현

---
+++ lib/data/repository_impl/city.repository_impl.dart
@@ -0,0 +1,17 @@
+import 'package:weather_app_flutter/data/data_source/local/city_list.dao.dart';
+import 'package:weather_app_flutter/data/mapper/city.mapper.dart';
+import 'package:weather_app_flutter/domain/model/city.model.dart';
+import 'package:weather_app_flutter/domain/repository/city.repository.dart';
+
+import '../dto/city_list.dto.dart';
+
+class CityRepositoryImpl implements CityRepository {
+  @override
+  Future<List<City>> getCities() async {
+    final rawData = await CityListDao.loadCityList();
+
+    final dtoList = rawData.map((json) => CityDto.fromJson(json)).toList();
+
+    return CityMapper.fromDtoList(dtoList);
+  }
+}
