d686d39d30bfd349328551330f670040761e41b6
constdreamcoder <scdreamricher@gmail.com>
2025-01-12 16:51:53 +0900

    [Refactor] 도시 목록 Mock 데이터로 리팩토링

--- test/Usecase/city.usecase_test.dart
+++ test/Usecase/city.usecase_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:weather_app_flutter/data/data_source/local/city_list.dao.dart';
+import 'package:weather_app_flutter/data/data_source/mock/mock_data/city_mock_data.dart';
 import 'package:weather_app_flutter/data/repository_impl/city.repository_impl.dart';
 import 'package:weather_app_flutter/domain/model/city/city.model.dart';
 import 'package:weather_app_flutter/domain/repository/city/city.repository.dart';
@@ -24,10 +25,8 @@ void main() {
   test('의존성 주입 성공 테슽', () => expect(cityUsecase, isNotNull));
 
   test('도시 목록 호출 테스트', () async {
-    final result = [
-      City(id: 707860, name: "Hurzuf", country: "UA", coord: Coord(lon: 34.283333, lat: 44.549999)),
-      City(id: 519188, name: "Novinki", country: "RU", coord: Coord(lon: 37.666668, lat: 55.683334)),
-    ];
+    final result = CityMockData.mockCityList;
+
     final usecase = MockGetCitiesUsecase();
 
     when(() => usecase(cityRepository)).thenAnswer((_) async => result);
